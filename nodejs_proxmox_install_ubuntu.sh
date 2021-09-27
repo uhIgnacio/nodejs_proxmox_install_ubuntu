15d298d8b27576c66e14ce34b1abc4ba5a9ff50f
igna <88955484+uhIgnacio@users.noreply.github.com>
2021-09-27 17:17:48 -0300

    ig this should work

--- nodejs_proxmox_install_ubuntu.sh
+++ nodejs_proxmox_install_ubuntu.sh
@@ -1,8 +1,10 @@
-echo "make sure this is run in root (sudo su)"
+#!/bin/bash
 
-apt-get update -y && apt-get upgrade -y
-apt-get install qemu-guest-agent
-systemctl start qemu-guest-agent
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+
+sudo apt-get update -y && apt-get upgrade -y
+sudo apt-get install qemu-guest-agent
+sudo systemctl start qemu-guest-agent
 echo "you may have to reboot"
 passwd
 echo "Setting the ssh config"
@@ -15,7 +17,6 @@ echo "all done thanks for using this script"
 
 echo "you can now exit the ssh and login as root user with the password you put"
 
-curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
-sudo apt install nodejs -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+nvm use node
 sudo apt-get install gcc g++ make
